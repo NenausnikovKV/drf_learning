cae44e445e1c063b03261ddf8add41d657302ee1
kostya <konstantin2113@mail.ru>
2024-02-01 17:16:04 +0300

    add post, put, delete to json test

--- json_snippets/tests.py
+++ json_snippets/tests.py
@@ -1,6 +1,7 @@
-import copy
 import io
+
 from django import shortcuts
+from django.db import transaction
 from django.test import TestCase, Client
 from rest_framework.parsers import JSONParser
 
@@ -47,7 +48,7 @@ class DjangoStyleViewFunctionTest(TestCase):
         self.assertIn("greeting", data)
         self.assertEquals(data, correct_data)
 
-    def test_json_snippet_list(self):
+    def test_get_snippet_list(self):
         """
             http http://127.0.0.1:8000/json/snippets/
         """
@@ -57,7 +58,38 @@ class DjangoStyleViewFunctionTest(TestCase):
         data = JSONParser().parse(content_reader)
         self.assertEquals(data, [self.full_data])
 
-    def test_json_snippet_detail(self):
+    def test_create_snippet(self):
+        """
+            http POST http://127.0.0.1:8000/json/snippets/ code="print(456)" name="assd" is_correct=true
+        """
+        address = shortcuts.reverse("json_snippets:snippet_list")
+
+
+        post_data = {
+            "name": "output_message",
+            "code": "print('hello')",
+            "is_correct": True,
+        }
+
+        response = self.client.post(address, data=post_data, content_type="application/json")
+        content_reader = io.BytesIO(response.content)
+        response_data = JSONParser().parse(content_reader)
+
+        self.assertIn("id", response_data)
+        self.assertIn("name", response_data)
+        self.assertIn("code", response_data)
+        self.assertIn("is_correct", response_data)
+
+        response_id = int(response_data["id"])
+        get_by_response_id_queryset = CodeSnippet.objects.filter(id=response_id)
+        self.assertTrue(get_by_response_id_queryset.exists())
+        CodeSnippet.objects.filter(id=response_id).delete()
+
+        with self.assertRaises(CodeSnippet.DoesNotExist):
+            wrong_id = 99999999
+            _ = CodeSnippet.objects.get(id=wrong_id)
+
+    def test_get_snippet_detail(self):
         """
             http http://127.0.0.1:8000/json/snippets/1
         """
@@ -69,38 +101,57 @@ class DjangoStyleViewFunctionTest(TestCase):
         self.assertIn("code", data)
         self.assertEquals(data, self.full_data)
 
-"""
-    http -a admin:password123 POST http://127.0.0.1:8000/snippets/ code="print(789)"
-"""
+    def test_put_snippet(self):
+        """
+            http PUT http://127.0.0.1:8000/json/snippets/ code="print(456)" name="assd" is_correct=true
+        """
 
-"""
-    http http://127.0.0.1:8000/simple_snippets/
+        # I choose huge id and hope testDB never creates so big table
+        big_id = 10266
+        with transaction.atomic():
+            CodeSnippet.objects.create(id=big_id, name="start_name", code="start code", is_correct=False)
 
-    http http://127.0.0.1:8000/simple_snippets/ Accept:application/json
-    http http://127.0.0.1:8000/simple_snippets.json
+        address = shortcuts.reverse("json_snippets:snippet_detail", kwargs={"pk": 1})
+        new_name = "new_name"
+        new_code = "new code"
+        new_is_correct = True
+        put_data = {
+            "id": big_id,
+            "name": new_name,
+            "code": new_code,
+            "is_correct": new_is_correct,
+        }
+        response = self.client.put(address, data=put_data, content_type="application/json")
+        content_reader = io.BytesIO(response.content)
+        response_data = JSONParser().parse(content_reader)
 
+        self.assertIn("id", response_data)
+        self.assertIn("name", response_data)
+        self.assertIn("code", response_data)
+        self.assertIn("is_correct", response_data)
 
-    http http://127.0.0.1:8000/simple_snippets/ Accept:text/html
-    http http://127.0.0.1:8000/simple_snippets.api
+        response_id = int(response_data["id"])
+        get_by_response_id_queryset = CodeSnippet.objects.filter(id=response_id)
+        self.assertTrue(get_by_response_id_queryset.exists())
+        self.assertEquals(response_data["name"], new_name)
+        self.assertEquals(response_data["code"], new_code)
+        self.assertEquals(response_data["is_correct"], new_is_correct)
+        CodeSnippet.objects.filter(id=big_id).delete()
 
-    http --form POST http://127.0.0.1:8000/simple_snippets/ code="print(123)"
-    http --json POST http://127.0.0.1:8000/simple_snippets/ code="print(456)"
-"""
-
-"""
-    http -a admin:password123 POST http://127.0.0.1:8000/snippets/ code="print(789)"
-"""
-"""
-
-    http http://127.0.0.1:8000/simple_snippets/
-
-    http http://127.0.0.1:8000/simple_snippets/ Accept:application/json
-    http http://127.0.0.1:8000/simple_snippets.json
-
-
-    http http://127.0.0.1:8000/simple_snippets/ Accept:text/html
-    http http://127.0.0.1:8000/simple_snippets.api
-
-    http --form POST http://127.0.0.1:8000/simple_snippets/ code="print(123)"
-    http --json POST http://127.0.0.1:8000/simple_snippets/ code="print(456)"
-"""
+    def test_delete_snippet(self):
+        """
+            http DELETE http://127.0.0.1:8000/json/snippets/1
+        """
+        # I choose huge id and hope testDB never creates so big table
+        big_id = 10267
+        with transaction.atomic():
+            CodeSnippet.objects.create(id=big_id, name="subtraction", code="k=7-9", is_correct=True)
+        big_id_queryset = CodeSnippet.objects.filter(id=big_id)
+        self.assertTrue(big_id_queryset.exists())
+        address = shortcuts.reverse("json_snippets:snippet_detail", kwargs={"pk": big_id})
+        response = self.client.delete(address)
+        self.assertEquals(response.status_code, 204)
+        self.assertFalse(big_id_queryset.exists())
+        with self.assertRaises(CodeSnippet.DoesNotExist):
+            wrong_id = big_id
+            _ = CodeSnippet.objects.get(id=wrong_id)
